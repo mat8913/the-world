529e05d6a569e753fe8eada462b2cea9888e765f
Matthew Harm Bekkema <mbekkema97@gmail.com>
2018-04-27 16:19:01 +1000

    Add mytdl command

---
+++ mytdl/Main.hs
@@ -0,0 +1,35 @@
+-- Copyright (C) 2018  Matthew Harm Bekkema
+--
+-- This file is part of the-world.
+--
+-- the-world is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- the-world is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+-----------------------------------------------------------------------------
+-- |
+-- Copyright   : Matthew Harm Bekkema 2018
+-- License     : GPL-3
+-- Maintainer  : mbekkema97@gmail.com
+-- Stability   : experimental
+--
+-- Runs youtube-dl, but removes UUIDs from any mkv files produced.
+-----------------------------------------------------------------------------
+
+import System.Environment (getArgs)
+
+import Media.MKV.UUID (removeUUIDsIn)
+import Web.YouTube    (Middleware (..), youtubeDl)
+
+
+main :: IO ()
+main = getArgs >>= youtubeDl "." (Middleware removeUUIDsIn)
